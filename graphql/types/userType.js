ea2b32bd01448bb2862966184db2d81fefc5fb46
Octi21 <80587811+Octi21@users.noreply.github.com>
2023-01-11 13:56:36 +0200

    Update userType.js

--- graphql/types/userType.js
+++ graphql/types/userType.js
@@ -2,18 +2,37 @@ const {
   GraphQLObjectType,
   GraphQLID,
   GraphQLString,
+  GraphQLList
 } = require('graphql');
+
+const models = require("../../models");
 
 
 const userType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => {
+    const orderType = require("./orderType")
+
+
+    return ({
       id: {
-          type: GraphQLID,
+        type: GraphQLID,
       },
       username: {
-          type: GraphQLString,
+        type: GraphQLString,
       },
+      orders: {
+        type: new GraphQLList(orderType),
+        resolve: (user) => {
+          return models.Order.findAll({
+            where: {
+              userId: user.id
+            }
+          })
+        }
+      }
+    })
+      
   }
 });
 
